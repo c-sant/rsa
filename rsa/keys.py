1b31261ed7af9be891313066e7f08de18fd1a149
Carlos Santos <cadu.vs@hotmail.com>
2024-09-11 11:22:11 -0300

    refactor: key generation algorithm

--- rsa/keys.py
+++ rsa/keys.py
@@ -15,8 +15,13 @@ def generate_keys(bits: int = 1024) -> tuple[int, int, int]:
 
     # chaves p e q devem usar metade dos bits para que o resultado de d e e tenha
     # o tamanho esperado (e.g. para chaves de 4 bits, usam-se duas chaves de 2 bits)
-    p = generate_prime(bits // 2)
-    q = generate_prime(bits // 2)
+    while True:
+        p = generate_prime(bits // 2)
+        q = generate_prime(bits // 2)
+
+        if p != q:
+            break
+
     n = p * q
     phi = (p - 1) * (q - 1)
     e = generate_coprime(phi, 2, phi - 1)
