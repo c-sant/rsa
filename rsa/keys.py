7beba93b56cac33d9d4855a13fc1fb1cd1c12c0b
Carlos Santos <cadu.vs@hotmail.com>
2024-09-11 08:26:31 -0300

    feat: key generation algorithm

---
+++ rsa/keys.py
@@ -0,0 +1,25 @@
+from .coprimes import generate_coprime
+from .primes import generate_prime
+
+
+def generate_keys(bits: int = 1024) -> tuple[int, int, int]:
+    """Gera chaves RSA do tamanho especificado.
+
+    Args:
+        bits (int): O tamanho esperado para as chaves RSA.
+
+    Returns:
+        tuple[int, int, int]: Os valores utilizados para encriptação, decriptação
+        e operação módulo, respectivamente.
+    """
+
+    # chaves p e q devem usar metade dos bits para que o resultado de d e e tenha
+    # o tamanho esperado (e.g. para chaves de 4 bits, usam-se duas chaves de 2 bits)
+    p = generate_prime(bits // 2)
+    q = generate_prime(bits // 2)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = generate_coprime(phi, 2, phi - 1)
+    d = pow(e, -1, phi)
+
+    return e, d, n
